432d80442f93411067220025e0072acee6576387
lushenle <lushenle@gmail.com>
2023-03-13 21:56:51 +0800

    add transfer api and currency valid

--- api/accounts.go
+++ api/accounts.go
@@ -8,13 +8,13 @@ import (
 	db "github.com/lushenle/simplebank/db/sqlc"
 )
 
-type createAccountParams struct {
+type createAccountRequest struct {
 	Owner    string `json:"owner" binding:"required"`
-	Currency string `json:"currency" binding:"required,oneof=USD EUR CAD"`
+	Currency string `json:"currency" binding:"required,currency"`
 }
 
 func (server *Server) createAccount(ctx *gin.Context) {
-	var req createAccountParams
+	var req createAccountRequest
 	if err := ctx.ShouldBindJSON(&req); err != nil {
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
